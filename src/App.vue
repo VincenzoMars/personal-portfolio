8ae1ab995ca626136181677255d91f2137f02cda
MVRS <vincenzo.mars16@gmail.com>
2022-07-05 15:35:34 +0200

    fix: deleted splash loader imports

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-<splash-loader></splash-loader>
 <router-view></router-view>
 </template>
 
 <script>
-import SplashLoader from './components/SplashLoader.vue'
 
 export default {
     name: 'app',
-    components: {
-        SplashLoader
-    }
 }
 </script>
