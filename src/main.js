bae0fd4b9b254f839708f74996180fb31eddcee5
MVRS <vincenzo.mars16@gmail.com>
2022-07-07 23:08:10 +0200

    fix: added things to config and main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import store from './store/index.js'
+import './scss/global/index.scss'
 
 const app = createApp(App)
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,4 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "./src/scss/global/_variables.scss";
-          @import "./src/scss/global/_utilities.scss";
-          @import "./src/scss/global/_animations.scss";
-          @import "./src/scss/global/_transitions.scss";
-          @import "./src/scss/global/_base.scss";
-        `
-      }
-    }
-  },
 })
