6d6e4dbad6c7e96533c252af105ab4a5005beb75
MVRS <vincenzo.mars16@gmail.com>
2022-07-07 23:08:43 +0200

    fix: added scss scaffolding

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,13 @@
 <template>
 <div>
-    <h1>daosda</h1>
-    <h1>dsaojdajs</h1>
+    <h1>Heading H1</h1>
+    <h2>Heading H2</h2>
+    <h3>Heading H3</h3>
+    <h4>Heading H4</h4>
+    <h5>Heading H5</h5>
+    <h6>Heading H6</h6>
+    <p>Paragraph</p>
+    <a href="">Anchor</a>
 </div>
 </template>
 
